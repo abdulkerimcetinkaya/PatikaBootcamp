d75a82a5c9d4f83614b21f37bc90ece0b47dea05
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-02-21 02:00:58 +0300

    Prime Number

--- PatikaBootcamp/src/Week1/PrimeNumber.java
+++ PatikaBootcamp/src/Week1/PrimeNumber.java
@@ -3,6 +3,8 @@ package Week1;
 public class PrimeNumber {
     public static void main(String[] args) {
 
+        // Printing prime number from 1 to 100
+
         for (int i = 2; i < 100; i++){
             boolean isPrime = true;
             for (int j = 2; j< (i-1); j++){
