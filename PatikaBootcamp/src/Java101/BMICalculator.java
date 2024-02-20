bcdf43c65a4c8e60f5055fa5a152f7ae959702f0
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-02-20 18:27:44 +0300

    BMI Calculator

---
+++ PatikaBootcamp/src/Java101/BMICalculator.java
@@ -0,0 +1,20 @@
+package Java101;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double weight, height, BMI;
+
+        System.out.print("Enter your weight (KG) : ");
+        weight = scan.nextDouble();
+        System.out.print("Enter your height (M) : ");
+        height = scan.nextDouble();
+
+        BMI = weight / (height * height);
+
+        System.out.println("BMI : " + BMI);
+    }
+}
