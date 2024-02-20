163c76bd96649647b7b9eef071a8f9165d1a8d2d
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-02-21 02:33:21 +0300

    Fibonacci (Eleman sayısı kullanıcıdan alındı)

--- PatikaBootcamp/src/Week1/Fibonacci.java
+++ PatikaBootcamp/src/Week1/Fibonacci.java
@@ -1,11 +1,19 @@
 package Week1;
 
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
+        int count = input.nextInt();
+
         int n1 = 0;
         int n2 = 1;
         int n3;
-        int count = 10;
+
 
         System.out.print(n1 + " " + n2);
 
