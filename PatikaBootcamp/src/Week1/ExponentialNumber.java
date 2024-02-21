50349d71ec3b92e73da39ae14eb1d1228eaee6b2
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-02-21 17:11:56 +0300

    Exponential Number Update

--- PatikaBootcamp/src/Week1/ExponentialNumber.java
+++ PatikaBootcamp/src/Week1/ExponentialNumber.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 
 public class ExponentialNumber {
     public static void main(String[] args) {
-        power(2,3);
-        power(5,7);
+
+        // Scanner sınıfı tanımlandı
+        Scanner input = new Scanner(System.in);
+
+        // kullanıcıdan taban sayısını al
+        System.out.print("Enter base number : ");
+        int num1 = input.nextInt();
+
+        // kullanıcıdan üs sayısını al
+        System.out.print("Enter exponental number : ");
+        int num2 = input.nextInt();
+        // sonucu ekrana yazdır
+        System.out.println("Result : " + power(num1,num2));
+
     }
 
-    public static void power(int base, int exponent){
-        int result = 1;
-
-        for (int i = 0; i < exponent;i++){
-            result *= base;
+    public static int power(int base, int exponent){
+        // If the exponent is equal to 0, return 1,
+        // otherwise compute the power recursively.
+        if (exponent == 0){
+            // Base case: if the exponent is 0, return 1
+            return 1;
+        } else {
+            // Recursive case: compute the power by multiplying the base
+            // with the result of recursively computing the power with
+            // a decremented exponent.
+            int result = base * power(base, exponent - 1);
+            return result;
         }
-
-        System.out.println("Result : " + result);
     }
+
 }
