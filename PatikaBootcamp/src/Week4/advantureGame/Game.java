0e9204013838ab67d01aa3e6ead54a8aee4bf7d5
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-03-07 01:55:20 +0300

    Bölge seçimi eklendi

--- PatikaBootcamp/src/Week4/advantureGame/Game.java
+++ PatikaBootcamp/src/Week4/advantureGame/Game.java
@@ -13,6 +13,27 @@ public class Game {
         System.out.println(player.getName() + " macera oyununa hoşgeldin!!");
         player.selectChar();
 
+        Location location = null;
+        while (true){
+            System.out.print("------------------BÖLGELER------------------\n1 - Güvenli Ev\n2 - Mağaza\nLütfen gitmek istediğiniz bölgeyi seçiniz : ");
+            int selectLoc = scan.nextInt();
+            switch (selectLoc){
+                case 1:
+                    location = new SafeHouse(player);
+                    break;
+                case 2:
+                    location = new ToolStore(player);
+                    break;
+                default:
+                    location = new SafeHouse(player);
+            }
+
+            if(!location.onLocation()){
+                System.out.println("GAME OVER");
+                break;
+            }
+        }
+
     }
 
 }
