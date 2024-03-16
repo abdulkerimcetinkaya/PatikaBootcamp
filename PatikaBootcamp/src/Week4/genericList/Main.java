ef7731ba40ad3bcbdb1e4b800a332de657263478
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-03-16 17:40:20 +0300

    Yorum satırları eklendi

--- PatikaBootcamp/src/Week4/genericList/Main.java
+++ PatikaBootcamp/src/Week4/genericList/Main.java
@@ -2,21 +2,12 @@ package Week4.genericList;
 
 public class Main {
     public static void main(String[] args) {
-        myList<Integer> list = new myList<>(1);
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(40);
-        list.add(50);
-        list.add(60);
-        list.add(70);
-        list.add(80);
-        list.add(90);
-        list.add(100);
-        list.get(2);
+        myList<String> list = new myList<>(20);
+        list.add("istanbul");
+        list.add("ankara");
+        list.getCapacity();
 
-        list.set(2,30);
-        list.print();
+
 
 
 
--- PatikaBootcamp/src/Week4/genericList/myList.java
+++ PatikaBootcamp/src/Week4/genericList/myList.java
@@ -8,26 +8,30 @@ public class myList<T> {
     private int capacity;
     private int size;
 
+    // Parametresiz constructor
     public myList(){
         this.array = (T[]) new Object[10];
     }
-
+    // Parametreli constructor
     public myList(int initialCapacity) {
         this.array = (T[]) new Object[initialCapacity];
         this.size = 0;
         this.capacity = initialCapacity;
     }
 
+    // Boyutu getir
     public int size() {
         System.out.println("Dizinin boyutu : " + size);
         return size;
     }
 
+    // kapasiteyi getir
     public int getCapacity(){
         System.out.println("Dizinin kapasitesi : " + capacity);
         return capacity;
     }
 
+    // Veriyi ekle
     public void add(T data){
         if (size == capacity){
             updateCapacity();
@@ -36,6 +40,7 @@ public class myList<T> {
         size++;
     }
 
+    // Kapasiteyi artır
     private void updateCapacity(){
         int newCapacity = capacity * 2;
         T[] newArray = Arrays.copyOf(array, newCapacity);
@@ -47,14 +52,14 @@ public class myList<T> {
         getCapacity();
         size();
     }
-
+    // verilen indexte ki değeri getirir.
     public int get(int index){
         if (array[index] != null){
             System.out.println(array[index]);
         }
         return index;
     }
-
+    // Verilen indexte ki değeri sil
     public void remove (int index){
         if (index < 0 || index >= size){
             System.out.println("Geçersiz indis numarası girdiniz");
@@ -69,18 +74,21 @@ public class myList<T> {
         size--;
     }
 
+    // Girilen veriyi girilen indexte ki değerle değiştirme
     public void set(int index, T data){
         array[index] = data;
     }
 
+
+    // Listeyi yazdırma
     @Override
     public String toString() {
-        for (int i = 0; i < size ; i++) {
+        for (int i = 0; i <size; i++) {
             System.out.println(array[i]);
         }
         return super.toString();
     }
-
+    // Grilen değerin index sırası (Baştan)
     public int indexOf(T data){
         for (int i = 0; i < size - 1; i++) {
             if (array[i].equals(data)){
@@ -90,7 +98,7 @@ public class myList<T> {
         }
         return -1;
     }
-
+    // Grilen değerin index sırası (Sondan)
     public int lastIndexOf(T data){
         for (int i = size-1; i >= 0; i--) {
             if (array[i].equals(data)){
@@ -101,14 +109,17 @@ public class myList<T> {
         return -1;
     }
 
+    //Boş mu?
     public boolean isEmpty(){
         return size == 0;
     }
 
+    // Yeni bir Array oluştur
     T[] toArray(){
         return Arrays.copyOf(array,capacity);
     }
 
+    // Listeyi temizle
     public void clear(){
         for (int i = 0; i < size; i++) {
             array[i] = null;
@@ -116,17 +127,18 @@ public class myList<T> {
         size = 0;
     }
 
-    myList<T> sublist(int start, int finish){
+    // belirtilen index noktaları aralığında yeni bir liste üret
+    public myList<T> sublist(int start, int finish){
         if (start < 0 || finish > size || start > finish){
-            System.out.println("Geçersi değer girdiniz");
+            System.out.println("Geçersiz değer girdiniz");
         }
-        myList<T> subList = new myList<>(finish - start);
+        myList<T> newList = new myList<>(finish - start);
         for (int i = start; i < finish; i++) {
-            subList.add(array[i]);
+            newList.add(array[i]);
         }
-        return subList;
+        return newList;
     }
-
+    // Liste verilen datayı içeriyor mu?
     public boolean contains(T data){
         for (int i = 0; i < size ; i++) {
             if (array[i] == data){
