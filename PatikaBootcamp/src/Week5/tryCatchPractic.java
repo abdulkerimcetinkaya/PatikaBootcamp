b942e2300b84c7be96f50d53b4bfe487fbc7d87c
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-03-18 17:45:08 +0300

    try catch pratiği güncellendi

--- PatikaBootcamp/src/Week5/tryCatchPractic.java
+++ PatikaBootcamp/src/Week5/tryCatchPractic.java
@@ -1,11 +1,17 @@
 package Week5;
 
+import java.util.Scanner;
+
 public class tryCatchPractic {
     public static void main(String[] args) {
         int[] list = new int[]{1,2,3,4,5,6,7,8,9,10};
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir indis numarası giriniz : ");
+        int i = scan.nextInt();
+
         try {
-            listElements(list, 2);
+            listElements(list, i);
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Hatalı giriş yaptınız.!!!");
         }
@@ -13,7 +19,7 @@ public class tryCatchPractic {
     }
 
     public static void listElements(int[] arr,int index){
-        System.out.println(arr[index]);
+        System.out.println("Girilen indisdeki sayı : "+arr[index]);
     }
 
 
