6f33d750361a89cbc8181a9b45aa05d70c2678d2
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-03-19 18:25:35 +0300

    Kitap sıralayıcı ödevi güncelleme

--- PatikaBootcamp/src/Week5/BookOrder/Main.java
+++ PatikaBootcamp/src/Week5/BookOrder/Main.java
@@ -1,35 +1,58 @@
 package Week5.BookOrder;
 
+import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        // Kitap isimlerine göre sıralama (Alfabeye göre)
-        TreeSet<Book> books = new TreeSet<>(new OrderNameComparable());
-        books.add(new Book("Olağanüstü Bir Gece",80,"Stefan Zweig",2017));
-        books.add(new Book("Dönüşüm",80,"Franz Kafka",2018));
-        books.add(new Book("Cürümenin Kitabı",192,"Emil Michel Cioran",2017));
-        books.add(new Book("Kitaplar ve Sigaralar",	64,"George Orwell",1946));
-        books.add(new Book("Insanlığımı Yitirirken",128,"Osamu Dazai",2022));
+        Scanner scan = new Scanner(System.in);
 
-        for(Book b : books){
+        System.out.println("1 -> Kitap Adına Göre Dizilim (Alfabetik Sıra)");
+        System.out.println("2 -> Sayfa Sayısına Göre Dizilim (Küçükten Büyüğe)");
+        System.out.println("Bir seçim yapınız : ");
+        int chosse = scan.nextInt();
+
+        switch (chosse){
+            case 1:
+                System.out.println("Kitapları isimlere göre sıralaması");
+                System.out.println("-----------------------------------");
+                toPageNumber();
+                break;
+            case 2:
+                System.out.println("Sayfa Sayısına Göre Dizilim (Küçükten Büyüğe)");
+                System.out.println("-----------------------------------");
+                toName();
+                break;
+            default:
+                System.out.println("Geçerli sayı giriniz : ");
+        }
+    }
+
+    public static void toPageNumber(){
+        // Kitap isimlerine göre sıralama (Alfabeye göre)
+        TreeSet<Book> orderBookName = new TreeSet<>(new OrderNameComparable());
+        orderBookName.add(new Book("Olağanüstü Bir Gece",80,"Stefan Zweig",2017));
+        orderBookName.add(new Book("Dönüşüm",80,"Franz Kafka",2018));
+        orderBookName.add(new Book("Cürümenin Kitabı",192,"Emil Michel Cioran",2017));
+        orderBookName.add(new Book("Kitaplar ve Sigaralar",	64,"George Orwell",1946));
+        orderBookName.add(new Book("Insanlığımı Yitirirken",128,"Osamu Dazai",2022));
+
+        for(Book b : orderBookName){
             System.out.println(b.getName()+ " -> Yazarı : " + b.getAuthorName());
         }
+    }
 
-        System.out.println("-------------------------");
-
+    public static void toName(){
         // Kitapları sayfa sayısına göre sıralama (Küçükten, büyüğe)
-        TreeSet<Book> book = new TreeSet<>(new OrderPageNumberComparable());
-        book.add(new Book("Olağanüstü Bir Gece",90,"Stefan Zweig",2017));
-        book.add(new Book("Dönüşüm",80,"Franz Kafka",2018));
-        book.add(new Book("Cürümenin Kitabı",192,"Emil Michel Cioran",2017));
-        book.add(new Book("Kitaplar ve Sigaralar",	64,"George Orwell",1946));
-        book.add(new Book("Insanlığımı Yitirirken",128,"Osamu Dazai",2022));
+        TreeSet<Book> orderPageNumber = new TreeSet<>(new OrderPageNumberComparable());
+        orderPageNumber.add(new Book("Olağanüstü Bir Gece",90,"Stefan Zweig",2017));
+        orderPageNumber.add(new Book("Dönüşüm",80,"Franz Kafka",2018));
+        orderPageNumber.add(new Book("Cürümenin Kitabı",192,"Emil Michel Cioran",2017));
+        orderPageNumber.add(new Book("Kitaplar ve Sigaralar",	64,"George Orwell",1946));
+        orderPageNumber.add(new Book("Insanlığımı Yitirirken",128,"Osamu Dazai",2022));
 
-        for(Book b2 : book){
+        for(Book b2 : orderPageNumber){
             System.out.println(b2.getName()+ " -> sayfa sayısı : " +b2.getPageNumber());
         }
-
-
     }
 }
--- PatikaBootcamp/src/Week5/BookOrder/OrderNameComparable.java
+++ PatikaBootcamp/src/Week5/BookOrder/OrderNameComparable.java
@@ -7,4 +7,5 @@ public class OrderNameComparable implements Comparator<Book> {
     public int compare(Book o1, Book o2) {
         return o1.getName().compareTo(o2.getName());
     }
+
 }
