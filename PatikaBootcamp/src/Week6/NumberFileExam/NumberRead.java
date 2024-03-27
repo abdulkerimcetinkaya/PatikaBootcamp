527d29e519fb8a27e0aa0e4a194f7a5bdcf7d602
abdulkerimcetinkaya <a.cetinkayaa78@gmail.com>
2024-03-28 01:49:42 +0300

    Dosyadan Sayıları Okuma ve Toplama

--- PatikaBootcamp/src/Week6/NumberFileExam/NumberRead.java
+++ PatikaBootcamp/src/Week6/NumberFileExam/NumberRead.java
@@ -3,20 +3,32 @@ package Week6.NumberFileExam;
 import java.io.*;
 
 public class NumberRead {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
+        String targetPath = "src/Week6/NumberFileExam/Number.txt";
+        try {
+            //createFile(targetPath);
+            writeFile(targetPath);
+            readFile(targetPath);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-        //-------------Dosya Oluşturma-------------------
-
-        // Bir dizin belirterek 'Number.txt' adında bir dosya oluşturuldu referans olarak 'file' nesnesine atandı.
-        File file = new File("src/Week6/NumberFileExam/Number.txt");
+    // ------------ Dosya Oluşturma(createFile) metodu ------------------------
+    public static void createFile(String path) throws IOException{
+        // dizin belirtilerek 'Number.txt' dosyası oluşturuldu ve 'file' nesnesine atandı
+        File file = new File(path);
         // Belirtilen dizinde dosyanın oluşup oluşmadığı kontrol edildi.
         if (file.createNewFile()){
             System.out.println(file.getName() + " dosyası oluşturuldu");
         }else {
             System.out.println(file.getName() + " dosyası zaten mevcut !");
         }
+    }
 
-        //-------------Dosya Yazma-------------------
+    // ------------ Dosyaya Yazma(fileWrite) metodu ------------------------
+    public static void writeFile(String path) throws IOException{
+        File file = new File(path);
 
         FileWriter fileWriter = new FileWriter(file,false);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
@@ -26,11 +38,11 @@ public class NumberRead {
         bufferedWriter.write("12");bufferedWriter.newLine();
         bufferedWriter.write("33");bufferedWriter.newLine();
         bufferedWriter.close();
+    }
 
-
-        //-------------Dosya Okuma-------------------
-
-
+    // ------------ Dosyayı Okuma(fileRead) metodu ------------------------
+    public static void readFile(String path)throws IOException{
+        File file = new File(path);
         FileReader fileReader = new FileReader(file);
         String line;
         int sum = 0;
@@ -41,14 +53,7 @@ public class NumberRead {
             sum += number;
 
         }
-        bufferedReader.close();
-
-
         System.out.println("Dosyadaki sayıların toplamı : " + sum);
-
-
-
-
-
+        bufferedReader.close();
     }
 }
